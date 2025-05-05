2a53ff9a45f2091cb88799c23f009439dbeed5c2
Poonam G <110119611+poonamg1524@users.noreply.github.com>
2025-05-05 20:18:58 +0530

    Update slack-notification.go
    commit 5- updated slack-notification file

--- send-jenkins-notification/slack-notification.go
+++ send-jenkins-notification/slack-notification.go
@@ -4,42 +4,42 @@ import (
 	"fmt"
 	"os"
 	"github.com/slack-go/slack"
-	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Get the Slack token from the Jenkins environment variables
+	token := os.Getenv("SLACK_BOT_TOKEN")
+	if token == "" {
+		fmt.Println("Slack Bot Token is missing!")
+		return
+	}
+	api := slack.New(token)
 
-	// err := godotenv.Load("E:/Web Tech/Go/slack-jenkins-go/.env")
-	// if err != nil {
-	// 	fmt.Println("Error loading .env:", err)
-	// }
+	// Get the command-line arguments passed to the script
 	args := os.Args[1:]
 	fmt.Println(args)
 
-	token := os.Getenv("SLACK_BOT_TOKEN")
-	// token := os.Getenv("SLACK_BOT_TOKEN")
-	if token == "" {
-		fmt.Println("Error: SLACK_BOT_TOKEN is not set in environment variables.")
+	// Ensure you have the right number of arguments
+	if len(args) < 4 {
+		fmt.Println("Insufficient arguments. Expected: URL, build result, build number, job name.")
 		return
 	}
 
-	api := slack.New(token)
-
-	//Introductory Message Components
+	// Introductory message components
 	preText := "*Hello! Your Jenkins Build has finished!*"
-	jenkinsURL := "*Build URL:* " + args[0] 
+	jenkinsURL := "*Build URL:* " + args[0]
 	buildResult := "*" + args[1] + "*"
 	buildNumber := "*" + args[2] + "*"
 	jobName := "*" + args[3] + "*"
 
-	//Adding ❌ if the build fails and ✅ if build is success
-
+	// Modify build result to include success or failure emoji
 	if buildResult == "*SUCCESS*" {
 		buildResult = buildResult + " :white_check_mark:"
 	} else {
 		buildResult = buildResult + " :x:"
 	}
 
+	// Slack message components
 	dividerSection1 := slack.NewDividerBlock()
 	jenkinsBuildDetails := jobName + " #" + buildNumber + " - " + buildResult + "\n" + jenkinsURL
 
@@ -54,9 +54,9 @@ func main() {
 		jenkinsBuildDetailsSection,
 	)
 
-	//send msg func returns channel,timestamp
+	// Send the message to Slack channel
 	_, _, _, errSendMsg := api.SendMessage(
-		"C08Q213RFAR", //SlackChannelID ,
+		"C08Q213RFAR", // SlackChannelID,
 		msg,
 	)
 
