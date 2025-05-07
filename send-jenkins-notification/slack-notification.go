b5a81d0de1174ebd8abae422757c4721750092d2
Poonam G <110119611+poonamg1524@users.noreply.github.com>
2025-05-07 16:41:08 +0530

    Update slack-notification.go
    Send Jenkins build status to Slack via HTTP POST

--- send-jenkins-notification/slack-notification.go
+++ send-jenkins-notification/slack-notification.go
@@ -3,21 +3,30 @@ package main
 import (
 	"fmt"
 	"os"
+
 	"github.com/slack-go/slack"
+
+	// "github.com/joho/godotenv"
+	"encoding/json"
+	"net/http"
 )
 
-func main() {
-	// Get the Slack token from the Jenkins environment variables
-	token := os.Getenv("SLACK_BOT_TOKEN")
-	if token == "" {
-		fmt.Println("Slack Bot Token is missing!")
-		return
-	}
-	api := slack.New(token)
+type jenkinsBuild struct {
+	BuildURL    string `json:"buildurl"`   
+	BuildResult string `json:"buildresult"`
+	BuildNumber string `json:"buildnumber"`
+	JobName     string `json:"jobname"`
+}
+
+// HandlerFunction
+func sendSlackMessage(w http.ResponseWriter, r *http.Request) {
+	// err := godotenv.Load("E:/Web Tech/Go/slack-jenkins-go/.env")
+	// if err != nil {
+	// 	fmt.Println("Error loading .env:", err)
+	// }
 
 	// Get the command-line arguments passed to the script
 	args := os.Args[1:]
-	fmt.Println(args)
 
 	// Ensure you have the right number of arguments
 	if len(args) < 4 {
@@ -25,21 +34,55 @@ func main() {
 		return
 	}
 
-	// Introductory message components
+	fmt.Println(args)
+
+	// // token := os.Getenv("SLACK_BOT_TOKEN") // this will take token from jenkins environment variable
+	// token := os.Getenv("SLACK_BOT_TOKEN")
+	// if token == "" {
+	// 	fmt.Println("Slack Bot Token is missing!")
+	// 	return
+	// }
+	// api := slack.New(token)
+
+	// //Introductory Message Components
+	// preText := "*Hello! Your Jenkins Build has finished!*"
+	// jenkinsURL := "*Build URL:* " + args[0]
+	// buildResult := "*" + args[1] + "*"
+	// buildNumber := "*" + args[2] + "*"
+	// jobName := "*" + args[3] + "*"
+
+	// //Adding ❌ if the build fails and ✅ if build is success
+	// if buildResult == "*SUCCESS*" {
+	// 	buildResult = buildResult + " :white_check_mark:"
+	// } else {
+	// 	buildResult = buildResult + " :x:"
+	// }
+
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "<h1>Sent Slack Message</h1>")
+
+	token := os.Getenv("SLACK_BOT_TOKEN")
+	if token == "" {
+		fmt.Println("Slack Bot Token is missing!")
+		return
+	}
+
+	build := jenkinsBuild{}
+	err0 := json.NewDecoder(r.Body).Decode(&build)
+	if err0 != nil {
+		http.Error(w, err0.Error(), http.StatusBadRequest)
+		return
+	}
+
+	api := slack.New(token)
+
 	preText := "*Hello! Your Jenkins Build has finished!*"
 	jenkinsURL := "*Build URL:* " + args[0]
 	buildResult := "*" + args[1] + "*"
 	buildNumber := "*" + args[2] + "*"
 	jobName := "*" + args[3] + "*"
 
-	// Modify build result to include success or failure emoji
-	if buildResult == "*SUCCESS*" {
-		buildResult = buildResult + " :white_check_mark:"
-	} else {
-		buildResult = buildResult + " :x:"
-	}
-
-	// Slack message components
+	// preText := "*Hello! Your Jenkins Build has finished!*"
 	dividerSection1 := slack.NewDividerBlock()
 	jenkinsBuildDetails := jobName + " #" + buildNumber + " - " + buildResult + "\n" + jenkinsURL
 
@@ -48,19 +91,25 @@ func main() {
 
 	jenkinsBuildDetailsSection := slack.NewSectionBlock(jenkinsBuildDetailsField, nil, nil)
 	preTextSection := slack.NewSectionBlock(preTextField, nil, nil)
+
 	msg := slack.MsgOptionBlocks(
 		preTextSection,
 		dividerSection1,
 		jenkinsBuildDetailsSection,
 	)
 
-	// Send the message to Slack channel
+	// send msg func returns channel, timestamp
 	_, _, _, errSendMsg := api.SendMessage(
-		"C08Q213RFAR", // SlackChannelID,
+		"C08Q213RFAR", // SlackChannelID
 		msg,
 	)
 
 	if errSendMsg != nil {
 		fmt.Printf("%s\n", errSendMsg)
 	}
+}
+
+func main() {
+	http.HandleFunc("/sendSlackMessage", sendSlackMessage)
+	http.ListenAndServe(":8091", nil)
 }
